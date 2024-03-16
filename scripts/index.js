530257637ee8ec8e192b9d4abe6d445d97de7bee
matt-colen <mzcolen@gmail.com>
2024-03-15 21:45:07 -0500

    Adds initial responsive grid layout

--- scripts/index.js
+++ scripts/index.js
@@ -7,16 +7,21 @@ const getPostHTML = (data) => {
     if (i === 0) {
       html += `
       <div class="hero-post">
-        <p class="post-date">${post.date}</p>
-        <h2 class="post-title">${post.title}</h2>
-        <p class="post-preview">
-          ${post.preview}
-        </p>
+        <img class="post-img hero-post__img" src="${post.img}">
+        <div class="hero-post__img-overlay"></div>
+        <div class="hero-post__content">
+          <p class="post-date">${post.date}</p>
+          <h2 class="post-title">${post.title}</h2>
+          <p class="post-preview">
+            ${post.preview}
+          </p>
+        </div>
       </div>
       `;
     } else {
       html += `
-      <div class="post">
+      <div class="post row-2">
+        <img class="post-img" src="${post.img}">
         <p class="post-date">${post.date}</p>
         <h2 class="post-title">${post.title}</h2>
         <p class="post-preview">
--- scripts/postData.js
+++ scripts/postData.js
@@ -3,36 +3,43 @@ const postData = [
     date: "July 23, 2022",
     title: "My new journey as a bootcamp student.",
     preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    img: `../assets/images/blog-image-hero.png`,
   },
   {
     date: "July 23, 2022",
     title: "Blog one",
     preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    img: "../assets/images/post-1.png",
   },
   {
     date: "July 23, 2022",
     title: "Blog two",
     preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    img: "../assets/images/post-2.png",
   },
   {
     date: "July 23, 2022",
     title: "Blog three",
     preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    img: "../assets/images/post-3.png",
   },
   {
     date: "July 23, 2022",
     title: "Blog four",
     preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    img: "../assets/images/post-4.png",
   },
   {
     date: "July 23, 2022",
     title: "Blog five",
     preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    img: "../assets/images/post-5.png",
   },
   {
     date: "July 23, 2022",
     title: "Blog six",
     preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    img: "../assets/images/post-6.png",
   },
 ];
 
