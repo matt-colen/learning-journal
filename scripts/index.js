4272969da90ddf4dfbef7ef0f972b24c6def2a90
matt-colen <mzcolen@gmail.com>
2024-03-16 00:21:15 -0500

    Initial home page build out

--- scripts/index.js
+++ scripts/index.js
@@ -10,10 +10,10 @@ const getPostHTML = (data) => {
       <div class="hero-post grid">
         <img class="post-img hero-post__img" src="${post.img}">
         <div class="hero-post__img-overlay"></div>
-        <div class="hero-post__content">
-          <p class="post-date">${post.date}</p>
-          <h2 class="post-title">${post.title}</h2>
-          <p class="post-preview">
+        <div class="hero-post__content flex flex--column">
+          <p class="post-date post-date--hero">${post.date}</p>
+          <h2 class="post-title post-title--hero">${post.title}</h2>
+          <p class="post-preview post-preview--hero">
             ${post.preview}
           </p>
         </div>
@@ -21,16 +21,18 @@ const getPostHTML = (data) => {
       `;
     } else {
       postsHTML += `
-      <div>
+      <div class="post__container">
         <div class="post-img__container">
           <img class="post-img" src="${post.img}">
           <div class="post-img__overlay"></div>
         </div>
         <p class="post-date">${post.date}</p>
-        <h2 class="post-title">${post.title}</h2>
-        <p class="post-preview">
-          ${post.preview}
-        </p>
+        <div class="post-details flex flex--column">
+          <h2 class="post-title">${post.title}</h2>
+          <p class="post-preview">
+            ${post.preview}
+          </p>
+        </div>
       </div>
       `;
     }
@@ -45,6 +47,15 @@ const getPostHTML = (data) => {
 };
 
 const renderPostHTML = () =>
-  (document.querySelector("#posts-section").innerHTML = getPostHTML(postData));
+  (document.querySelector("#posts-container").innerHTML =
+    getPostHTML(postData));
 
 renderPostHTML();
+
+const renderYear = () => {
+  document.querySelector(
+    "#footer__date"
+  ).innerHTML = `Copyright &copy ${new Date().getFullYear()}`;
+};
+
+renderYear();
--- scripts/postData.js
+++ scripts/postData.js
@@ -2,7 +2,7 @@ const postData = [
   {
     date: "July 23, 2022",
     title: "My new journey as a bootcamp student.",
-    preview: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    preview: `After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.`,
     img: `../assets/images/blog-image-hero.png`,
   },
   {
