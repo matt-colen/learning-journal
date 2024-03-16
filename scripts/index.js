656a46b277daaca2030d4a4b4127cc2b17253c2e
matt-colen <mzcolen@gmail.com>
2024-03-15 23:32:52 -0500

    Fixes responsive layout for posts section

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
 import postData from "./postData.js";
 
 const getPostHTML = (data) => {
-  let html = "";
+  let heroHTML = "";
+  let postsHTML = "";
 
   data.forEach((post, i) => {
     if (i === 0) {
-      html += `
-      <div class="hero-post">
+      heroHTML += `
+      <div class="hero-post grid">
         <img class="post-img hero-post__img" src="${post.img}">
         <div class="hero-post__img-overlay"></div>
         <div class="hero-post__content">
@@ -19,9 +20,12 @@ const getPostHTML = (data) => {
       </div>
       `;
     } else {
-      html += `
-      <div class="post row-2">
-        <img class="post-img" src="${post.img}">
+      postsHTML += `
+      <div>
+        <div class="post-img__container">
+          <img class="post-img" src="${post.img}">
+          <div class="post-img__overlay"></div>
+        </div>
         <p class="post-date">${post.date}</p>
         <h2 class="post-title">${post.title}</h2>
         <p class="post-preview">
@@ -32,7 +36,12 @@ const getPostHTML = (data) => {
     }
   });
 
-  return html;
+  const combinedHTML = `
+    ${heroHTML}
+    <div class="additional-posts grid">${postsHTML}</div>
+  `;
+
+  return combinedHTML;
 };
 
 const renderPostHTML = () =>
